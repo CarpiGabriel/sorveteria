fe8bdfe6faf564437b5b0799f0130602ac78f734
Pc Gamer <gabrielcarpirodrigues@gmail.com>
2025-05-20 13:32:15 -0300

    Deixando bonito 5

--- src/main/java/br/edu/fesa/sorveteria/model/Cliente.java
+++ src/main/java/br/edu/fesa/sorveteria/model/Cliente.java
@@ -16,10 +16,18 @@ public class Cliente {
     private String email;
     private String telefone;
 
+    // Novos campos para endereço
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String complemento;
+
     public Cliente() {
     }
 
     // Getters e Setters
+
     public Long getId() {
         return id;
     }
@@ -50,5 +58,45 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
     }
 }
